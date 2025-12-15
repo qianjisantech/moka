15cf5602fafde48b20373d224d0c76ee7a998cec
xiaoyuanyuan-rush <2497822530@qq.com>
2025-12-16 01:29:52 +0800

    fix：新增后端3

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://139.196.208.166/api',
         changeOrigin: true
       }
     }
